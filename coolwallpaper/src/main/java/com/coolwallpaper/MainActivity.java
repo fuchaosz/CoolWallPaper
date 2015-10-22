238a8530df252bd6d437de6d6cc1fcee7b994e85
fuchaosz <chaogemail@126.com>
2015-10-22 18:14:27 +0800

    研究图片的缩放和平移

--- coolwallpaper/src/main/java/com/coolwallpaper/MainActivity.java
+++ coolwallpaper/src/main/java/com/coolwallpaper/MainActivity.java
@@ -1,26 +1,56 @@
 package com.coolwallpaper;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 
-public class MainActivity extends ActionBarActivity {
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private PhotoViewAttacher attacher;
+    private float maxMoveLength;
+    private float centerX;
+    private float centerY;
+    private float moveLength;
+    private Matrix matrix;
 
     @ViewInject(R.id.tv_hollo)
     TextView helloworld;
+
+    @ViewInject(R.id.image)
+    ImageView image;
+
+    @ViewInject(R.id.btn_left)
+    Button btnLeft;
+
+    @ViewInject(R.id.btn_right)
+    Button btnRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ViewUtils.inject(this);
+        //添加监听器
+        this.addListener();
+        //调整图片
+        this.adjustPicture();
+    }
+
+    //添加监听器
+    private void addListener() {
     }
 
     @Override
@@ -47,10 +77,48 @@ public class MainActivity extends ActionBarActivity {
 
     @OnClick(R.id.btn_show_pic)
     public void onClick(View v) {
-        //跳转到显示图片的地方
-        // ShowPictureActivity.startActivity(this);
-        ShowPictureListActivity.startActivity(this);
-        finish();
+        switch (v.getId()) {
+            case R.id.btn_show_pic:
+                //跳转到显示图片的地方
+                // ShowPictureActivity.startActivity(this);
+                ShowPictureListActivity.startActivity(this);
+                finish();
+                break;
+        }
+
+    }
+
+    //调整图片
+    private void adjustPicture() {
+        this.matrix = image.getImageMatrix();
+        //获取屏幕大小
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        //获取图片的实际大小
+        int drawWidth = image.getDrawable().getIntrinsicWidth();
+        int drawHeight = image.getDrawable().getIntrinsicHeight();
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+        matrix.postScale(screenHeight * 1.0f / drawHeight, screenHeight * 1.0f / drawHeight);
+        image.setImageMatrix(matrix);
+        this.maxMoveLength = (this.image.getDrawable().getBounds().width() - screenWidth) / 2.0f;
+        this.centerX = screenWidth / 2;
+        this.centerY = screenHeight / 2;
+        //图片右移
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveLength += 20;
+                matrix.preTranslate(moveLength, 0);
+                image.setImageMatrix(matrix);
+            }
+        });
+
+        //        attacher = new PhotoViewAttacher(image);
+        //        float scale = screenHeight * 1.0f / drawHeight;
+        //        attacher.setScaleLevels(scale, scale * attacher.getMediumScale(), scale * attacher.getMaximumScale());
+        //        attacher.setScale(scale);
+        //        attacher.update();
     }
 
 }
--- coolwallpaper/src/main/java/com/coolwallpaper/ShowPictureDetailActivity.java
+++ coolwallpaper/src/main/java/com/coolwallpaper/ShowPictureDetailActivity.java
@@ -2,9 +2,12 @@ package com.coolwallpaper;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.coolwallpaper.activity.BaseActivity;
 import com.coolwallpaper.bean.PictureBean;
@@ -12,6 +15,10 @@ import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 /**
  * 显示图片详情
@@ -21,6 +28,7 @@ public class ShowPictureDetailActivity extends BaseActivity implements View.OnCl
 
     private PictureBean pictureBean;
     private ImageLoader imageLoader;
+    private PhotoViewAttacher attacher;//图片缩放的控件
 
     @ViewInject(R.id.iv_image)
     ImageView ivImage;
@@ -28,6 +36,9 @@ public class ShowPictureDetailActivity extends BaseActivity implements View.OnCl
     //浮动层菜单
     @ViewInject(R.id.ly_picture_detail_menu)
     View lyPictureDetailMenu;
+
+    @ViewInject(R.id.pb_progress)
+    ProgressBar progressBar;
 
     /**
      * 启动方法，要显示的图片
@@ -55,8 +66,37 @@ public class ShowPictureDetailActivity extends BaseActivity implements View.OnCl
         //创建ImageLoader
         this.imageLoader = ImageLoader.getInstance();
         this.imageLoader.init(ImageLoaderConfiguration.createDefault(this));
+        //获取屏幕大小
+        final DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        //设置ImageView
+        this.ivImage.setMaxHeight(dm.heightPixels);
+        this.ivImage.setMaxWidth(2 * dm.widthPixels);//最大宽度为屏幕的2倍
+        this.attacher = new PhotoViewAttacher(ivImage);
         //显示图片
-        this.imageLoader.displayImage(pictureBean.getImageUrl(), ivImage);
+        this.imageLoader.displayImage(pictureBean.getImageUrl(), ivImage, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String s, View view) {
+
+            }
+
+            @Override
+            public void onLoadingFailed(String s, View view, FailReason failReason) {
+
+            }
+
+            @Override
+            public void onLoadingComplete(String s, View view, Bitmap bitmap) {
+                //调整图片，使得图片的高度与屏幕一样高
+                scalePictureToScreenHeight();
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingCancelled(String s, View view) {
+
+            }
+        });
     }
 
     @OnClick(R.id.iv_image)
@@ -68,5 +108,21 @@ public class ShowPictureDetailActivity extends BaseActivity implements View.OnCl
                 lyPictureDetailMenu.setVisibility(View.VISIBLE);
                 break;
         }
+    }
+
+    //放大图片
+    private void scalePictureToScreenHeight() {
+        //获取view大小
+        int viewWidth = ivImage.getWidth() - ivImage.getPaddingLeft() - ivImage.getPaddingRight();
+        int viewHeight = ivImage.getHeight() - ivImage.getPaddingTop() - ivImage.getPaddingBottom();
+        //获取图片的实际大小
+        int drawWidth = ivImage.getDrawable().getIntrinsicWidth();
+        int drawHeight = ivImage.getDrawable().getIntrinsicHeight();
+        //        //获取Matrix
+        //        Matrix matrix = ivImage.getImageMatrix();
+        //        matrix.postScale(viewHeight * 1.0f / drawHeight, viewHeight * 1.0f / drawHeight);
+        //        ivImage.setImageMatrix(matrix);
+        this.attacher.setScale(viewHeight * 1.0f / drawHeight);
+        this.attacher.update();
     }
 }
